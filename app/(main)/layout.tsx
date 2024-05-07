9068e976a74b34554e846e939bd172e810c78a54
insu <hinsu93@gmail.com>
2024-05-07 13:53:35 +0900

    sidebar 고정시키기

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "@/components/sidebar";
+
 type Props = {
   children: React.ReactNode;
 };
@@ -5,7 +7,10 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <div>{children}</div>
+      <Sidebar />
+      <main className="pl-[256px] h-full">
+        <div className="bg-red-500 h-full">{children}</div>
+      </main>
     </>
   );
 };
