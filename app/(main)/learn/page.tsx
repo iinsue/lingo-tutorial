8b2cd1738e0b52efc363fb41f1b8d71c0fcc8069
insu <hinsu93@gmail.com>
2024-05-07 15:56:13 +0900

    Add Sticky Wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,11 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
 const LearnPage = () => {
   return (
     <>
-      <div>Learn Page</div>
+      <div className="flex flex-row-reverse gap-[48px] px-6">
+        <StickyWrapper>My Sticky Sidebar</StickyWrapper>
+      </div>
     </>
   );
 };
