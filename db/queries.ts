f9b671fc231046bca28746dc28d3c4bfcc509d36
insu <hinsu93@gmail.com>
2024-05-08 15:12:44 +0900

    User progress 설정

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { userProgress } from "./schema";
+import { courses, userProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 export const getCourses = cache(async () => {
@@ -21,6 +21,15 @@ export const getUserProgress = cache(async () => {
     with: {
       activeCourse: true,
     },
+  });
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    // TDOO: Populate units and lessons
   });
 
   return data;
