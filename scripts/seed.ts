0d5336b98d8b382bf58ead1248e4408cdfa5efde
insu <hinsu93@gmail.com>
2024-05-09 11:27:40 +0900

    update seed.tsx

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,12 +14,32 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       { id: 1, title: "Spanish", imageSrc: "/es.svg" },
       { id: 2, title: "Italian", imageSrc: "/it.svg" },
       { id: 3, title: "French", imageSrc: "/fr.svg" },
       { id: 4, title: "Croatian", imageSrc: "/hr.svg" },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      { id: 1, unitId: 1, order: 1, title: "Nouns" },
+      { id: 2, unitId: 1, order: 2, title: "Verbs" },
     ]);
 
     console.log("Seeding finished");
