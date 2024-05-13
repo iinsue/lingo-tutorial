f632e4b896c0ef72e7fda6c6a8ccd200ae73515c
insu <hinsu93@gmail.com>
2024-05-13 12:38:45 +0900

    Update lesson-button.tsx

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Check, Crown, Star } from "lucide-react";
+
 type Props = {
   id: number;
   index: number;
@@ -9,10 +11,41 @@ type Props = {
   percentage: number;
 };
 
-export const LessonButton = ({}: Props) => {
+export const LessonButton = ({
+  id,
+  index,
+  totalCount,
+  locked,
+  current,
+  percentage,
+}: Props) => {
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
+
+  let indentationLevel;
+
+  if (cycleIndex <= 2) {
+    indentationLevel = cycleIndex;
+  } else if (cycleIndex <= 4) {
+    indentationLevel = 4 - cycleIndex;
+  } else if (cycleIndex <= 6) {
+    indentationLevel = 4 - cycleIndex;
+  } else {
+    indentationLevel = cycleLength - 8;
+  }
+
+  const rightPosition = indentationLevel * 40;
+
+  const isFirst = index === 0;
+  const isLast = index === totalCount;
+  const isCompleted = !current && !locked;
+
+  const Icon = isCompleted ? Check : isLast ? Crown : Star;
+  const href = isCompleted ? `/lesson/${id}` : "/lesson";
+
   return (
     <>
-      <div>Lesson Button</div>
+      <div>{id}</div>
     </>
   );
 };
