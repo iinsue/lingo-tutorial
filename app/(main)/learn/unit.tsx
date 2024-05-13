1a6778e0c6cb9299ea2a88b57b2efb898ccf1d11
insu <hinsu93@gmail.com>
2024-05-13 13:12:46 +0900

    update lesson button

--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -32,7 +32,8 @@ export const Unit = ({
       <UnitBanner title={title} description={description} />
       <div className="flex items-center flex-col relative">
         {lessons.map((lesson, index) => {
-          const isCurrent = lesson.id === activeLesson?.id;
+          const isCurrent = true || lesson.id === activeLesson?.id;
+          // TODO: Remove true later
           const isLocked = !lesson.completed && !isCurrent;
           return (
             <>
