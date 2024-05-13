253881eb90bd407757844e902bdb62dc3486e0a8
insu <hinsu93@gmail.com>
2024-05-13 15:23:38 +0900

    add header component

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { challengeOptions, challenges } from "@/db/schema";
+import { useState } from "react";
+import { Header } from "./header";
 
 type Props = {
   initialPercentage: number;
@@ -20,9 +22,16 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialHearts);
+
   return (
     <>
-      <div>Quiz!</div>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
     </>
   );
 };
