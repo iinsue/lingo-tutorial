188b6f838454e7f259436d389d30dff07664657e
insu <hinsu93@gmail.com>
2024-05-14 09:45:30 +0900

    add question bubble

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,7 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
 
 type Props = {
   initialPercentage: number;
@@ -29,8 +30,15 @@ export const Quiz = ({
     const uncompletedIndex = challenges.findIndex(
       (challenge) => !challenge.completed
     );
-    return uncompletedIndex;
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
+
+  const challenge = challenges[activeIndex];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
   return (
     <>
@@ -43,9 +51,13 @@ export const Quiz = ({
         <div className="h-full flex items-center justify-center">
           <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
             <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
-              Which of these is an apple?
+              {title}
             </h1>
-            <div></div>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+            </div>
           </div>
         </div>
       </div>
