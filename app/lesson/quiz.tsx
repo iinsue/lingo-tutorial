8826421103450969dd5e281c2ed574877b6504c8
insu <hinsu93@gmail.com>
2024-05-14 15:27:15 +0900

    문제 맞거나 틀릴 때 사운드 추가

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -9,6 +9,7 @@ import { Footer } from "./footer";
 import { upsertChallengeProgress } from "@/actions/challenge-progress";
 import { toast } from "sonner";
 import { reduceHearts } from "@/actions/user-progress";
+import { useAudio } from "react-use";
 
 type Props = {
   initialPercentage: number;
@@ -28,6 +29,10 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [correctAudio, _c, correctControls] = useAudio({ src: "/correct.mp3" });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({
+    src: "/incorrect.mp3",
+  });
   const [pending, startTransition] = useTransition();
 
   const [hearts, setHearts] = useState(initialHearts);
@@ -84,6 +89,8 @@ export const Quiz = ({
               console.error("Missing hearts");
               return;
             }
+
+            correctControls.play();
             setStatus("correct");
             setPercentage((prev) => prev + 100 / challenges.length);
 
@@ -103,6 +110,7 @@ export const Quiz = ({
               return;
             }
 
+            incorrectControls.play();
             setStatus("wrong");
 
             if (!response?.error) {
@@ -121,6 +129,8 @@ export const Quiz = ({
 
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
