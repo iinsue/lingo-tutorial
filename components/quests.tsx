98fe45e7911d9e2e82c61f7ba4069e585aaf5a4f
insu <hinsu93@gmail.com>
2024-05-16 14:46:30 +0900

    add quest page

---
+++ components/quests.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+type Props = {
+  points: number;
+};
+
+export const Quests = ({ points }: Props) => {
+  return (
+    <>
+      <div className="border-2 rounded-xl p-4 space-y-4">
+        <div className="flex items-center justify-between w-full">
+          <h3 className="font-bold text-lg">Quests</h3>
+          <Link href="/quests">
+            <Button size="sm" variant="primaryOutline">
+              View all
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
