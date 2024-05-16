6d89625997b9a8cae9963738017d005cf0c3f56b
insu <hinsu93@gmail.com>
2024-05-16 13:54:27 +0900

    Update page.tsx

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const LeaderBoardPage = async () => {
   const userProgressData = getUserProgress();
@@ -48,8 +50,24 @@ const LeaderBoardPage = async () => {
           <p className="text-muted-foreground text-center text-lg mb-6">
             See where you stand among other learners in the community.
           </p>
+          <Separator className="mb-4 h-0.5 rounded-full" />
           {leaderboard.map((userProgress, index) => (
-            <div key={userProgress.userId}>{userProgress.userName}</div>
+            <div
+              key={userProgress.userId}
+              className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+            >
+              <p className="font-bold text-lime-700 mr-4">{index + 1}</p>
+              <Avatar className="border bg-green-500 h-12 w-12 ml-3 mr-6">
+                <AvatarImage
+                  className="object-cover"
+                  src={userProgress.userImageSrc}
+                />
+              </Avatar>
+              <p className="font-bold text-neutral-800 flex-1">
+                {userProgress.userName}
+              </p>
+              <p className="text-muted-foreground">{userProgress.points} XP</p>
+            </div>
           ))}
         </FeedWrapper>
       </div>
