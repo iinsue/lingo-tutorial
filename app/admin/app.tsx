f5ea4a0dde0ca8f8714c2063e86519dcdef6f386
insu <hinsu93@gmail.com>
2024-05-16 16:46:38 +0900

    add unit crud

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -5,6 +5,9 @@ import simpleRestProvider from "ra-data-simple-rest";
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
 import { CourseEdit } from "./course/edit";
+import { UnitList } from "./units/list";
+import { UnitCreate } from "./units/create";
+import { UnitEdit } from "./units/edit";
 
 const dataProvider = simpleRestProvider("/api");
 
@@ -18,6 +21,13 @@ const App = () => {
           list={CourseList}
           create={CourseCreate}
           edit={CourseEdit}
+        />
+        <Resource
+          name="units"
+          recordRepresentation="title"
+          list={UnitList}
+          create={UnitCreate}
+          edit={UnitEdit}
         />
       </Admin>
     </>
