658fcbcea8b4fe2f754c224ca08c4fd555dd45ff
insu <hinsu93@gmail.com>
2024-05-16 16:28:47 +0900

    add admin edit & delete

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -4,6 +4,7 @@ import { Admin, Resource } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
 
 const dataProvider = simpleRestProvider("/api");
 
@@ -16,6 +17,7 @@ const App = () => {
           recordRepresentation="title"
           list={CourseList}
           create={CourseCreate}
+          edit={CourseEdit}
         />
       </Admin>
     </>
